0c5d3d6fac2b2d2316bfaa12b765364d2ecf7a15
xentra-gabrielynigojavierto <gabriel.javierto@xentragroup.com>
2026-03-31 14:11:37 +0000

    reviewing git again

--- service-a/program.cs
+++ service-a/program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello DevOps TEAM! test 2 21/11/2025");
+app.MapGet("/", () => "Hello DevOps TEAM! test  31/03/2026");
 
 app.Run();
